f7f2a0c9c009ec31bd4a6bcc3703bcac6834cd5f
Srishti Kamra <srishtikamra07@gmail.com>
2022-11-13 23:07:56 -0600

    Initial Commit

---
+++ sim/simApp.swift
@@ -0,0 +1,17 @@
+//
+//  simApp.swift
+//  sim
+//
+//  Created by Srishti Kamra  on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct simApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
